5ca44d7d0300d99d967ecf906a6ac72d6cff9d94
Hepheir <hepheir@gmail.com>
2023-08-06 03:14:51 +0900

    feat(feed): create Post model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from user.models import User
+
 # Create your models here.
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True)
+    content = models.CharField(max_length=2000)
+    tagged_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
