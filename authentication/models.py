d54852b4dc22f5c02b738c3a6cae76bb929f131e
Hepheir <hepheir@gmail.com>
2023-08-06 03:23:15 +0900

    feat(authentication): create EmailValidation model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EmailValidation(models.Model):
+    email = models.EmailField(primary_key=True, max_length=64)
+    code = models.CharField(max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True)
