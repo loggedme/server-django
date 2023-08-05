11831613410907391d742b88c8a743702334ec38
Hepheir <hepheir@gmail.com>
2023-08-06 03:17:45 +0900

    feat(user): create User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserType(models.IntegerChoices):
+    PERSONAL = 1, 'Personal'
+    BUSINESS = 2, 'Business'
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    email = models.EmailField(unique=True, max_length=64)
+    password = models.CharField(max_length=16, blank=False)
+    name = models.CharField(max_length=16, blank=False)
+    handle = models.CharField(unique=True, max_length=16, blank=False)
+    account_type = models.IntegerField(choices=UserType.choices)
+    profile_image = models.Field()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_logged_in = models.DateTimeField(null=True)
