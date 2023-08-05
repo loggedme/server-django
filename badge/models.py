092f47516e5fb6977cd200854729c6605670fc60
Hepheir <hepheir@gmail.com>
2023-08-06 03:18:14 +0900

    feat(badge): create Badge model

--- badge/models.py
+++ badge/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from user.models import User
+
 # Create your models here.
+
+
+class Badge(models.Model):
+    id = models.UUIDField(primary_key=True)
+    image = models.Field()
+    description = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
