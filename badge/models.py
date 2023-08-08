e7e5ea6392dd6055e9bba0e14e192f4b403baeeb
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-08 19:44:51 +0900

    feat: Add BadgedUser, FollowedUser

--- badge/models.py
+++ badge/models.py
@@ -11,3 +11,7 @@ class Badge(models.Model):
     description = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class BadgedUser(models.Model):
+    badge_id = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)     # 뱃지를 부여 받은 사람
