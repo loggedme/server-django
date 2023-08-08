6a06f89898c3644f5a47390b12423f2c31b7ad43
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-09 02:24:50 +0900

    feat: Add urls

--- badge/urls.py
+++ badge/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    # path('badge/', ...)
+    path('badge', views.BadgeApi.as_view()),
+    path('badge/<str:badge_id>', views.BadgeApi.as_view()),
+    path('/user/<str:user.id>/badge', views.UserBadgeApi.as_view()),
+    path('/user/<str:user.id>/badge/<str:badge.id>', views.UserBadgeApi.as_view()),
 ]
