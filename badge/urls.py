ed8b627580a4680f98a3c0c764ad95d89a2d0bb5
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-09 18:19:40 +0900

    fix: urls.py

--- badge/urls.py
+++ badge/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('badge', views.BadgeApi.as_view()),
     path('badge/<str:badge_id>', views.BadgeApi.as_view()),
-    path('/user/<str:user.id>/badge', views.UserBadgeApi.as_view()),
-    path('/user/<str:user.id>/badge/<str:badge.id>', views.UserBadgeApi.as_view()),
+    path('user/<str:user_id>/badge', views.UserBadgeApi.as_view()),
+    path('user/<str:user_id>/badge/<str:badge_id>', views.UserBadgeApi.as_view()),
 ]
