9a25cc9bacbd7a3c8369c748c1441c8e22594781
Hepheir <hepheir@gmail.com>
2023-08-11 23:05:03 +0900

    fix(config): DRF의 DEFAULT_AUTHENTICATION_CLASSES 를 JWTAuthentication으로 정정

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
