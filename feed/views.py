035890fb2cb91f3abb96077db9c34de57c443564
Hepheir <hepheir@gmail.com>
2023-08-12 07:27:24 +0900

    feat(feed): impl GET "/feed" (but without query param supports)

--- feed/views.py
+++ feed/views.py
@@ -1,12 +1,28 @@
+from typing import Any
 from uuid import UUID
 
 from django.http import HttpRequest
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+
+from feed.models import Post
+from feed.pagination import SimplePagination
+from feed.serializers import PostSerializer
 
 
 class FeedView(APIView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.pagination = SimplePagination()
+
+    @permission_classes([AllowAny])
     def get(self, request: HttpRequest):
-        pass
+        # TODO: Support Query parameters
+        queryset = Post.objects.all()
+        page = self.pagination.paginate_queryset(queryset, request, view=self)
+        serializer = PostSerializer(instance=page, many=True)
+        return self.pagination.get_paginated_response(serializer.data)
 
     def post(self, request: HttpRequest):
         pass
