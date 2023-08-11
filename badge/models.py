1e00fe363bd76a63a1194eef95e446bec12814cc
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-11 23:26:14 +0900

    refactor(badge): image 필드 수정

--- badge/models.py
+++ badge/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 from user.models import User
 
-# Create your models here.
-
-
 class Badge(models.Model):
     id = models.UUIDField(primary_key=True)
-    image = models.Field()
+    image = models.ImageField(upload_to='badge_image/')
     description = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
