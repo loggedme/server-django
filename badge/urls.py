99bd1fa3a6452e6e59fc4b0e73259917c27da2f6
Hepheir <hepheir@gmail.com>
2023-08-12 03:28:24 +0900

    feat(badge): 새로 변경된 배지 부여/회수 api에 맞도록 api 수정

--- badge/urls.py
+++ badge/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('badge', views.BadgeApi.as_view()),
     path('badge/<str:badge_id>', views.BadgeApi.as_view()),
-    path('user/<str:user_id>/badge', views.UserBadgeApi.as_view()),
-    path('user/<str:user_id>/badge/<str:badge_id>', views.UserBadgeApi.as_view()),
+    path('badge/<str:badge_id>/user', views.BadgeUserApi.as_view()),
 ]
