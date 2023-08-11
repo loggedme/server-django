45775720e88b625bbf196956e90575cb589e44fe
Hepheir <hepheir@gmail.com>
2023-08-11 19:47:11 +0900

    feat(feed): add SimplePagination to pagination.py

---
+++ feed/pagination.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, Iterable
+
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class SimplePagination(PageNumberPagination):
+    page_size = 50
+    page_query_param = 'page'
+
+    def get_paginated_response(self, data: Iterable[Dict | Any]):
+        return Response({
+            'count': self.page.paginator.count,
+            'is_countable': True,
+            'page': self.page.number,
+            'per_page': self.page.paginator.per_page,
+            'items': data,
+        })
