194fdf0bb90ce8923fec11effd076d90c7513a02
Hepheir <hepheir@gmail.com>
2023-08-11 19:48:56 +0900

    feat(feed): add PostSerializer in serializers.py

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,1 +1,16 @@
 from rest_framework import serializers
+
+from feed.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'content',
+            'tagged_user',
+            'created_at',
+            'modified_at',
+            'created_by',
+        ]
