d46cedb68bf7cebedb916050b20fbbf5f6635564
Hepheir <hepheir@gmail.com>
2023-08-13 23:01:35 +0900

    ci(tools): create deploy.sh

---
+++ tools/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPOSITORY_DIRECTORY=$(dirname "$(dirname "$0")")
+
+cd $REPOSITORY_DIRECTORY
+git fetch
+git checkout origin/master
+pip install -r requirements.txt
+python3 manage.py test
+python3 manage.py runserver 0:80
