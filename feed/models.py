5c3306f63580bd6ff1f04f04c14995670e7d6958
Hepheir <hepheir@gmail.com>
2023-08-14 06:17:47 +0900

    fix(feed): 동일 포스트, 동일 유저가 좋아요를 여러개 생성할 수 있는 버그 수정

--- feed/models.py
+++ feed/models.py
@@ -33,6 +33,9 @@ class LikedPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = [['user', 'post']]
+
 
 class SavedPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
