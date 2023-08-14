81312e07e72f21f22664eb9c71a6239ad56e24a3
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-14 14:42:08 +0900

    refactor(user): models.py
    User id에 default 추가, first_name, last_name 필드 삭제, FollowedUser 필드 이름 변경

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import uuid
+
 
 class UserType(models.IntegerChoices):
     PERSONAL = 1, 'Personal'
@@ -8,17 +10,19 @@ class UserType(models.IntegerChoices):
 
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, null=False, blank=False)
     name = models.CharField(max_length=16, blank=False)
     handle = models.CharField(unique=True, max_length=16, blank=False)
     account_type = models.IntegerField(choices=UserType.choices)
-    profile_image =  models.ImageField(upload_to='user_profile_image/')
+    profile_image =  models.ImageField(upload_to='user_profile_image/', null=True)
+    first_name = None
+    last_name = None
 
 
 class FollowedUser(models.Model):
-    user_id = models.ForeignKey(User, related_name='followed_user', on_delete=models.CASCADE)         # 팔로우 받는 사람
+    user = models.ForeignKey(User, related_name='followed_user', on_delete=models.CASCADE)         # 팔로우 받는 사람
     followed_by = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)     # 팔로우 하는 사람
 
     class Meta:
-        unique_together = ['user_id', 'followed_by']
+        unique_together = ['user', 'followed_by']
