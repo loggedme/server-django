6044797327bf82feca45aed20e4cca20fd43ec4b
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-08-14 19:19:03 +0900

    refactor(user): 게시물 저장 url 추가

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('user/<str:user_id>/following', views.FollowingListView.as_view()),
     path('user/<str:user_id>/following/<str:following>', views.FollowCreateDeleteView.as_view()),
     path('user/<str:user_id>/follower', views.FollowerListView.as_view()),
-    # path('user/<str:user_id>/saved', views.SavedPostListView.as_view()),
-    # path('user/<str:user_id>/saved/<str:feed_id>', views.SavedPostCreateDeleteView.as_view()),
+    path('user/<str:user_id>/saved', views.SavedPostListView.as_view()),
+    path('user/<str:user_id>/saved/<str:feed_id>', views.SavedPostCreateDeleteView.as_view()),
 ]
