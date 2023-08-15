84ad2418c848a632bca4e6109ccb2f400f749d3d
Hepheir <hepheir@gmail.com>
2023-08-16 00:18:54 +0900

    feat(user): add is_following to UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,14 +1,17 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
+from django.http import HttpRequest
 from rest_framework import serializers
-from user.models import User
+
+from user.models import *
 
 class UserSerializer(serializers.ModelSerializer):
     thumbnail = serializers.SerializerMethodField()
+    is_following = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'password', 'email', 'name', 'handle', 'account_type', 'profile_image', 'thumbnail']
+        fields = ['id', 'password', 'email', 'name', 'handle', 'account_type', 'profile_image', 'thumbnail', 'is_following']
         extra_kwargs = {
             'id': {'read_only': True},
             'email': {'write_only': True},
@@ -23,3 +26,19 @@ class UserSerializer(serializers.ModelSerializer):
         if obj.profile_image:
             return settings.HOSTNAME + obj.profile_image.url
         return None
+
+    def get_is_following(self, obj: User):
+        request: HttpRequest = self.context['request']
+        if request is None:
+            raise NotImplementedError()
+        user: AbstractBaseUser | AnonymousUser | User = request.user
+        if user is None:
+            raise NotImplementedError()
+        if user.is_anonymous:
+            return False
+        try:
+            user.follower.get(user=obj)
+            return True
+        except FollowedUser.DoesNotExist:
+            return False
+        return False
