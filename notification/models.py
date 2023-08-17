d03bc93bd6435248822d7889f80e6d6bac1aa6bc
Hepheir <hepheir@gmail.com>
2023-08-17 21:57:40 +0900

    feat(notification): add default value for id of Notification model

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
@@ -14,7 +16,7 @@ class NotificationType(models.IntegerChoices):
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     type = models.IntegerField(choices=NotificationType.choices)
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    post = models.ForeignKey('feed.Post', on_delete=models.CASCADE, null=True)
-    badge = models.ForeignKey('badge.Badge', on_delete=models.CASCADE, null=True)
+    arg_user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True)
+    arg_post = models.ForeignKey('feed.Post', on_delete=models.CASCADE, null=True)
+    arg_badge = models.ForeignKey('badge.Badge', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
