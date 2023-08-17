d44b9c2bb2b134feb96fc90222dc25be25bc310c
Hepheir <hepheir@gmail.com>
2023-08-17 22:21:26 +0900

    feat(notification): add column 'notified_user' to Notification

--- notification/models.py
+++ notification/models.py
@@ -16,6 +16,7 @@ class NotificationType(models.IntegerChoices):
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     type = models.IntegerField(choices=NotificationType.choices)
+    notified_user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='notifications')
     arg_user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True)
     arg_post = models.ForeignKey('feed.Post', on_delete=models.CASCADE, null=True)
     arg_badge = models.ForeignKey('badge.Badge', on_delete=models.CASCADE, null=True)
